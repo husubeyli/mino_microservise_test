7d628a43e831d7fb7a0e10b913dcd839fffcc259
Elvin <59554164+husubayli-elvin@users.noreply.github.com>
2021-01-08 13:16:01 +0400

    sss

---
+++ mini_microservice_project_export/front_service/scripts.js
@@ -0,0 +1,84 @@
+
+async function renderPost(post){
+    // let post_comments = await getCommentData(post.id);
+    let comments = post.comments;
+    let id = post.id
+    return `<div class="col-md-3">
+                <div class="card p-4" >
+
+                    <h5 class="card-title">${post.title}</h5>
+                      <div class="card-body">
+                        <ul>
+                            ${ comments ? comments.map(comment => `<li>${comment.content} </li>`) : '' }
+                        </ul>
+                          <form id="createComment" onsubmit='createComment(event, id)' data-id="form_${id}">
+                              <label>Comment</label>
+                              <input type="text" name="content"/>
+                          </form>
+                      </div>
+
+                </div>
+            </div>`
+}
+
+async function getPostData() {
+    let response = await fetch('http://localhost:5002/posts/',)
+    return await response.json();
+}
+
+// async function getCommentData(id) {
+//     let response = await fetch(`http://localhost:5001/posts/${id}/comments/`,)
+//     return await response.json();
+// }
+
+
+async function createPost(e){
+    e.preventDefault();
+    let form = e.target;
+    let postData = {
+        'title': form.title.value
+    }
+    let response = await fetch('http://localhost:5003/posts/',{
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(postData),
+    })
+    let createPostData = await response.json();
+    console.log(createPostData);
+    let htmlContent =  await renderPost(createPostData);
+    document.querySelector('#postList').innerHTML += htmlContent;
+}
+
+
+async function createComment(e, id){
+   console.log('salam')
+    e.preventDefault()
+    let form = e.target;
+    let data = {
+        'title': form.content.value
+    }
+    console.log(data)
+    console.log(id)
+    let response = await fetch(`http://localhost:5001/posts/${id}/comments/`,{
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+    })
+
+}
+
+
+window.onload = async () => {
+
+    let postList  = await getPostData();
+    let htmlContent = ''
+    for (post of postList){
+        htmlContent += await renderPost(post)
+    }
+    document.querySelector('#postList').innerHTML += htmlContent;
+
+}
